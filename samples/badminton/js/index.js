e93049b5505223a812f6d3ec27f5858ee3e9f852
AOYAMA <dongqs@leyantech.com>
2024-10-06 04:33:19 +0800

    640

--- samples/badminton/js/index.js
+++ samples/badminton/js/index.js
@@ -4,11 +4,7 @@ let qvga = {width: {exact: 320}, height: {exact: 240}};
 
 let vga = {width: {exact: 640}, height: {exact: 480}};
 
-let fhd = {width: {ideal: 1920}, height: {ideal: 1080}};
-
-let resolution = fhd;
-
-//let resolution = window.innerWidth < 640 ? qvga : vga;
+let resolution = window.innerWidth < 640 ? qvga : vga;
 
 // whether streaming video from the camera.
 let streaming = false;
